0e280fa866a4ae37abad34caae3d562fb397d932
tw1zzR <regenuss@gmail.com>
2024-09-21 15:17:11 +0300

    Completed LAB 4.2

--- lab4.2.py
+++ lab4.2.py
@@ -1,2 +1,10 @@
-y=1
-dada = 0
+import math
+
+k = float(input("Введіть k: "))
+a = float(input("Введіть a: "))
+b = float(input("Введіть b: "))
+x = float(input("Введіть x: "))
+e = math.e
+
+W = ( (6 * pow(k,3) - math.sin(a) + pow(math.cos(b),2)) * math.sqrt(math.fabs(b)) + (pow(e,x) / 3 * pow(a,7)) )
+print(f"Значення виразу W = {round(W, 6)}.")
